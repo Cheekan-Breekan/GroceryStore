280d79f2576e5dfe41b43a13112d8a6bac12d630
MrSega13 <SegaKupr@mail.ru>
2024-04-23 16:14:32 +0300

    Infrastructure: fully transferred from test project.

---
+++ GroceryStore.Infrastructure/EmailSender.cs
@@ -0,0 +1,40 @@
+﻿using GroceryStore.Core.Interfaces;
+using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Logging;
+using MimeKit;
+
+namespace GroceryStore.Infrastructure;
+public class EmailSender(ILogger<EmailSender> logger) : IEmailSender
+{
+    public async Task<bool> SendEmailAsync(string email, string subject, string htmlMessage)
+    {
+        try
+        {
+            using var emailMessage = new MimeMessage();
+
+            emailMessage.From.Add(new MailboxAddress("Администрация сайта TestAppMarket", "SegaKupr@mail.ru"));
+            emailMessage.To.Add(new MailboxAddress("", email));
+            emailMessage.Subject = subject;
+            emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
+            {
+                Text = htmlMessage
+            };
+            using (var client = new SmtpClient())
+            {
+                client.ServerCertificateValidationCallback = (s, c, h, e) => true;
+                await client.ConnectAsync("smtp.mail.ru", 465, SecureSocketOptions.SslOnConnect);
+                await client.AuthenticateAsync("SegaKupr@mail.ru", "ygCETkpJ5vtvdyVMuRXt");
+                await client.SendAsync(emailMessage);
+
+                await client.DisconnectAsync(true);
+            }
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, ex.Message);
+            return false;
+        }
+    }
+}
---
+++ GroceryStore.Infrastructure/FileService.cs
@@ -0,0 +1,53 @@
+﻿using GroceryStore.Core.Interfaces;
+using Microsoft.AspNetCore.Http;
+
+namespace GroceryStore.Infrastructure;
+public class FileService : IFileService
+{
+    private const string ImagesFolder = "Images";
+    private static string[] AccessibleExtensions => ["jpg", "png", "jpeg", "bmp", "jfif", "gif"];
+    public string UploadImage(IFormFile file)
+    {
+        if (file is null)
+        {
+            throw new ArgumentNullException("File is null!");
+        }
+        var extension = file.FileName.Split(".").Last();
+        if (!AccessibleExtensions.Any(x => x == extension))
+        {
+            throw new ArgumentException("Extension not allowed!");
+        }
+        var savedFileName = Guid.NewGuid().ToString() + "." + extension;
+        Directory.CreateDirectory(GetStoragePath());
+        var savedFilePath = Path.Combine(GetStoragePath(), savedFileName);
+
+        using (var stream = new FileStream(savedFilePath, FileMode.Create))
+        {
+            file.CopyTo(stream);
+        }
+
+        return savedFileName;
+    }
+    public byte[] GetImage(string fileName)
+    {
+        if (string.IsNullOrEmpty(fileName))
+        {
+            return [];
+        }
+        if (!File.Exists(Path.Combine(GetStoragePath(), fileName)))
+        {
+            return [];
+        }
+        return File.ReadAllBytes(Path.Combine(GetStoragePath(), fileName));
+    }
+    public void DeleteImage(string fileName)
+    {
+        var path = Path.Combine(GetStoragePath(), fileName);
+        if (!File.Exists(path))
+        {
+            throw new ArgumentException($"File {fileName} not found!");
+        }
+        File.Delete(path);
+    }
+    private static string GetStoragePath() => Path.Combine(AppDomain.CurrentDomain.BaseDirectory, ImagesFolder);
+}
